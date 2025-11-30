b3c3bfaf6b45360251bf154aa1ef1a78760d526f
AndrewHKautuka <andrew.h.kautuka@gmail.com>
2025-11-30 23:21:35 +0200

    Add APP_SIDEBAR_ITEMS constant for sidebar navigation
    - Introduced a new APP_SIDEBAR_ITEMS constant in app-sidebar-items.ts to define the sidebar navigation items, including the Map title, URL, and icon.
    - This addition enhances the sidebar navigation structure and improves the overall user interface of the application.

---
+++ src/shared/constants/app-sidebar-items.ts
@@ -0,0 +1,11 @@
+import { MapIcon } from "lucide-react"
+
+import { SidebarNavItem } from "../types/app-sidebar-types"
+
+export const APP_SIDEBAR_ITEMS: SidebarNavItem[] = [
+  {
+    title: "Map",
+    url: "/map",
+    icon: MapIcon,
+  },
+]
