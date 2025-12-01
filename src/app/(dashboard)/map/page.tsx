905aff03e7b822a3badb76c2064b7405fdce063f
AndrewHKautuka <andrew.h.kautuka@gmail.com>
2025-12-01 12:12:34 +0200

    Update MapPage layout to include MapOptionsCard for enhanced user interaction
    - Integrated the MapOptionsCard component alongside the MalawiMap for improved map configuration options.
    - Adjusted the layout for better responsiveness and spacing, ensuring a more user-friendly interface.

--- src/app/(dashboard)/map/page.tsx
+++ src/app/(dashboard)/map/page.tsx
@@ -1,10 +1,12 @@
 import { MalawiMap } from "~/map/components/malawi-map"
+import { MapOptionsCard } from "~/map/components/map-options-card"
 
 export default async function MapPage() {
   return (
     <div className="flex items-center justify-center">
-      <div className="mx-16 flex h-full w-full flex-row gap-8">
-        <MalawiMap className="border-foreground aspect-video w-full border-2" />
+      <div className="mx-8 mt-8 flex h-full w-full flex-row items-center gap-8">
+        <MalawiMap className="border-foreground aspect-video flex-1 border-2" />
+        <MapOptionsCard className="h-fit w-[25%] min-w-fit" />
       </div>
     </div>
   )
