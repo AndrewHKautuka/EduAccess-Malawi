5ca27ba0b4ec2bfcfae687a2aa880f6a833bba3d
AndrewHKautuka <andrew.h.kautuka@gmail.com>
2025-12-01 10:16:12 +0200

    Refactor layout of MapPage component for improved responsiveness
    - Adjusted the structure of the MapPage component to enhance layout flexibility.
    - Wrapped the MalawiMap component in a div with flex properties to better manage spacing and alignment.

--- src/app/(dashboard)/map/page.tsx
+++ src/app/(dashboard)/map/page.tsx
@@ -2,8 +2,10 @@ import { MalawiMap } from "~/map/components/malawi-map"
 
 export default async function MapPage() {
   return (
-    <div className="flex h-full w-full items-center justify-center">
-      <MalawiMap className="border-foreground mx-16 aspect-video w-full border-2" />
+    <div className="flex items-center justify-center">
+      <div className="mx-16 flex h-full w-full flex-row gap-8">
+        <MalawiMap className="border-foreground aspect-video w-full border-2" />
+      </div>
     </div>
   )
 }
