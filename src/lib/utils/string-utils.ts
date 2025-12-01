972819350e3eb7bbfb837cfb6d346449d04fd88f
AndrewHKautuka <andrew.h.kautuka@gmail.com>
2025-12-01 22:04:54 +0200

    Add joinStringsHumanReadable function for improved string manipulation
    - Introduced joinStringsHumanReadable function to concatenate an array of strings into a human-readable format with specified separators.
    - Enhanced handling for empty and single-element arrays to ensure robust output.
    - This addition improves utility for string formatting within the application.

--- src/lib/utils/string-utils.ts
+++ src/lib/utils/string-utils.ts
@@ -7,3 +7,21 @@ export function capitalizeFirstLetter(
 
   return inputString.charAt(0).toUpperCase() + inputString.slice(1)
 }
+
+export function joinStringsHumanReadable(
+  endingSeparator: string,
+  strings: readonly string[],
+  mainSeparator?: string
+): string {
+  if (strings.length === 0) {
+    return ""
+  }
+
+  if (strings.length === 1) {
+    return strings[0]
+  }
+
+  const joinedStrings = strings.toSpliced(-1).join(mainSeparator)
+
+  return `${joinedStrings}${endingSeparator}${strings[-1]}`
+}
