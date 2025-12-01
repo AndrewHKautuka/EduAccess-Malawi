a56a57c1000ca1e964a8f886c4ccaba7e44be0d3
AndrewHKautuka <andrew.h.kautuka@gmail.com>
2025-12-01 07:29:09 +0200

    Enhance AppSidebar with theme selection functionality
    - Integrated theme selection into the AppSidebar by adding a ThemeDropdownMenu component, allowing users to switch between light, dark, and system themes.
    - Utilized the useTheme hook to manage the current theme and display the appropriate icon based on the selected theme.
    - Improved the sidebar structure by adding a new "System Settings" group for better organization of settings options.

--- src/shared/components/app-sidebar.tsx
+++ src/shared/components/app-sidebar.tsx
@@ -1,19 +1,33 @@
+"use client"
+
+import { useTheme } from "next-themes"
 import Link from "next/link"
+
+import { LucideIcon, MoonIcon, SunIcon, SunMoonIcon } from "lucide-react"
 
 import {
   Sidebar,
   SidebarContent,
+  SidebarFooter,
   SidebarGroup,
   SidebarGroupContent,
   SidebarGroupLabel,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
+  SidebarSeparator,
 } from "@/components/ui/sidebar"
+import { Theme } from "@/types/theme-types"
 
 import { APP_SIDEBAR_ITEMS } from "../constants/app-sidebar-items"
+import { ThemeDropdownMenu } from "./theme-dropdown-menu"
 
 export function AppSidebar() {
+  const { theme, setTheme } = useTheme()
+
+  const ThemeIcon: LucideIcon =
+    theme === "light" ? SunIcon : theme === "dark" ? MoonIcon : SunMoonIcon
+
   return (
     <Sidebar collapsible="icon">
       <SidebarContent>
@@ -34,7 +48,22 @@ export function AppSidebar() {
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
+        <SidebarSeparator />
+        <SidebarGroup>
+          <SidebarGroupLabel>System Settings</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenuItem>
+              <ThemeDropdownMenu theme={theme as Theme} setTheme={setTheme}>
+                <SidebarMenuButton className="cursor-pointer">
+                  <ThemeIcon />
+                  <span>Theme</span>
+                </SidebarMenuButton>
+              </ThemeDropdownMenu>
+            </SidebarMenuItem>
+          </SidebarGroupContent>
+        </SidebarGroup>
       </SidebarContent>
+      <SidebarFooter />
     </Sidebar>
   )
 }
