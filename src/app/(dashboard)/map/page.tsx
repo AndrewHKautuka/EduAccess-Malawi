0c59c7c9570e53b140157936ccb84ecde77884fa
AndrewHKautuka <andrew.h.kautuka@gmail.com>
2025-12-02 09:36:29 +0200

    Enhance MapPage and MalawiMap to support dynamic vector layers
    - Updated MapPage to fetch and handle the Malawi layer using the getMalawiLayer function, including error handling for data retrieval.
    - Modified MalawiMap to accept vectorLayers as a prop, allowing for dynamic rendering of geographic features based on the selected layers.
    - Improved integration with useMapOptionsStore for managing displayed layers, enhancing the overall functionality and user experience of the map component.

--- src/app/(dashboard)/map/page.tsx
+++ src/app/(dashboard)/map/page.tsx
@@ -1,11 +1,34 @@
 import { MalawiMap } from "~/map/components/malawi-map"
 import { MapOptionsCard } from "~/map/components/map-options-card"
+import { getMalawiLayer } from "~/map/data/map-data"
+
+import { VectorLayer } from "@/shared/types/layer-types"
 
 export default async function MapPage() {
+  console.log("Fetching Malawi Layer")
+
+  const malawiLayerResult = await getMalawiLayer()
+
+  if (!malawiLayerResult.success) {
+    console.error("Failed to fetch Malawi Layer")
+    return (
+      <div className="text-red-600">
+        {malawiLayerResult.error.message}
+        <br />
+        {malawiLayerResult.message}
+      </div>
+    )
+  }
+
+  const vectorLayers: VectorLayer[] = [malawiLayerResult.data]
+
   return (
     <div className="flex items-center justify-center">
       <div className="mx-8 mt-8 flex h-full w-full flex-row items-center gap-8">
-        <MalawiMap className="border-foreground aspect-video flex-1 border-2" />
+        <MalawiMap
+          vectorLayers={vectorLayers}
+          className="border-foreground aspect-video flex-1 border-2"
+        />
         <MapOptionsCard className="h-fit w-[25%] min-w-fit" />
       </div>
     </div>
--- src/modules/map/components/malawi-map.tsx
+++ src/modules/map/components/malawi-map.tsx
@@ -1,4 +1,8 @@
+"use client"
+
 import dynamic from "next/dynamic"
+
+import { VectorLayer } from "@/shared/types/layer-types"
 
 import {
   MAP_BOUNDS,
@@ -6,18 +10,29 @@ import {
   MAP_INITIAL_ZOOM,
   MAP_MIN_ZOOM,
 } from "../constants/malawi-map-constants"
+import { useMapOptionsStore } from "../hooks/use-map-options-store"
 
 interface MalawiMapProps {
+  vectorLayers: VectorLayer[]
   className?: string
 }
 
-export function MalawiMap({ className }: MalawiMapProps) {
-  const Map = dynamic(() => import("./map"))
+const Map = dynamic(() => import("./map"), { ssr: false })
+
+export function MalawiMap({ vectorLayers, className }: MalawiMapProps) {
+  const { adminBoundaryLevel } = useMapOptionsStore()
+
+  const malawiLayer = vectorLayers.find((layer) => layer.layerName === "malawi")
+
+  const layersToDisplay = [
+    ...(adminBoundaryLevel === "National" && malawiLayer ? [malawiLayer] : []),
+  ]
 
   return (
     <Map
       position={MAP_INITIAL_POSITION}
       bounds={MAP_BOUNDS}
+      vectorLayers={layersToDisplay}
       zoom={MAP_INITIAL_ZOOM}
       minZoom={MAP_MIN_ZOOM}
       className={className}
