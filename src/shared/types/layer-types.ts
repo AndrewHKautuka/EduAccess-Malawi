3738b7ba76031ff4b94672046a19837fe86173cb
AndrewHKautuka <andrew.h.kautuka@gmail.com>
2025-12-02 12:42:10 +0200

    Enhance layer-types.ts with VectorLayerConfig and VectorLayerWithConfig interfaces
    - Added VectorLayerConfig interface to define configuration options for vector layers, including table and column specifications.
    - Introduced VectorLayerWithConfig interface to extend the existing VectorLayer interface, incorporating configuration details for improved layer management.
    - This update enhances type safety and clarity for handling vector layers in the application.

--- src/shared/types/layer-types.ts
+++ src/shared/types/layer-types.ts
@@ -1,3 +1,7 @@
+import { PgColumn } from "drizzle-orm/pg-core"
+
+import { DatabaseQueryable } from "@/types/db-types"
+
 export interface GeoJSONFeature {
   type: "Feature"
   id: string | number
@@ -16,4 +20,22 @@ export interface GeoJSONFeatureCollection {
 export interface VectorLayer {
   layerName: string
   data: GeoJSONFeatureCollection
+}
+
+export interface VectorLayerConfig {
+  table: DatabaseQueryable
+  idColumn: PgColumn
+  geometryColumn: PgColumn
+  defaultStyle: {
+    color?: string
+    weight?: number
+    opacity?: number
+    fillColor?: string
+    fillOpacity?: number
+    radius?: number
+  }
+}
+
+export interface VectorLayerWithConfig extends VectorLayer {
+  config: VectorLayerConfig
 }
