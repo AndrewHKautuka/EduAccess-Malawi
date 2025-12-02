200808cba4fcb0b599ae4e34023902ab680150d0
AndrewHKautuka <andrew.h.kautuka@gmail.com>
2025-12-02 09:35:16 +0200

    Add vector layer support to Map component
    - Enhanced the Map component to accept and render multiple vector layers using GeoJSON.
    - Introduced a new prop, vectorLayers, to manage the display of geographic features with customizable styles and popups.
    - This update improves the map's functionality by allowing dynamic layer rendering based on provided GeoJSON data.

--- src/modules/map/components/map.tsx
+++ src/modules/map/components/map.tsx
@@ -2,7 +2,9 @@
 
 import { LatLngBoundsExpression, LatLngExpression } from "leaflet"
 import "leaflet-defaulticon-compatibility"
-import { MapContainer, TileLayer } from "react-leaflet"
+import { GeoJSON, MapContainer, TileLayer } from "react-leaflet"
+
+import { VectorLayer } from "@/shared/types/layer-types"
 
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css"
 import "leaflet/dist/leaflet.css"
@@ -10,6 +12,7 @@ import "leaflet/dist/leaflet.css"
 interface MapProps {
   position: LatLngExpression
   bounds?: LatLngBoundsExpression
+  vectorLayers?: VectorLayer[]
   zoom: number
   minZoom?: number
   className?: string
@@ -18,6 +21,7 @@ interface MapProps {
 export default function Map({
   position,
   bounds,
+  vectorLayers = [],
   zoom,
   minZoom,
   className,
@@ -34,6 +38,24 @@ export default function Map({
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+
+      {vectorLayers.map((layer) => (
+        <GeoJSON
+          key={layer.layerName}
+          data={layer.data}
+          style={(feature) => ({
+            color: feature?.geometry.type === "Polygon" ? "purple" : "black",
+            weight: 2,
+            opacity: 0.8,
+            fillOpacity: 0.5,
+          })}
+          onEachFeature={(feature, layer) => {
+            if (feature.properties && feature.properties.popupContent) {
+              layer.bindPopup(feature.properties.popupContent)
+            }
+          }}
+        />
+      ))}
     </MapContainer>
   )
 }
