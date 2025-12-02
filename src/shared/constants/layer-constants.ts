9472f2c1d2b2377a9b5788bef8a0f9193f7e5f90
AndrewHKautuka <andrew.h.kautuka@gmail.com>
2025-12-02 12:43:44 +0200

    Add layer-constants.ts to define available vector layers and their configurations
    - Introduced a new file, layer-constants.ts, to define the AVAILABLE_LAYERS constant, which includes configurations for various vector layers such as malawi, regions, districts, and more.
    - Each layer configuration specifies the associated database table, ID and geometry columns, and default styling options, enhancing the management of geographic data in the application.

---
+++ src/shared/constants/layer-constants.ts
@@ -0,0 +1,90 @@
+import {
+  districts,
+  districtSubdivisions,
+  educationFacilities,
+  malawi,
+  populatedPlaces,
+  regions,
+  roads,
+} from "@/lib/db/schema"
+
+import { VectorLayerConfig } from "../types/layer-types"
+
+export const AVAILABLE_LAYERS: Record<string, VectorLayerConfig> = {
+  malawi: {
+    table: malawi,
+    idColumn: malawi.id,
+    geometryColumn: malawi.geom,
+    defaultStyle: {
+      color: "#3388ff",
+      weight: 2,
+      fillOpacity: 0,
+    },
+  },
+  regions: {
+    table: regions,
+    idColumn: regions.id,
+    geometryColumn: regions.geom,
+    defaultStyle: {
+      color: "#ff7800",
+      weight: 2,
+      opacity: 0.8,
+      fillColor: "#ff7800",
+      fillOpacity: 0.2,
+    },
+  },
+  districts: {
+    table: districts,
+    idColumn: districts.id,
+    geometryColumn: districts.geom,
+    defaultStyle: {
+      color: "#ff7800",
+      weight: 2,
+      opacity: 0.8,
+      fillColor: "#ff7800",
+      fillOpacity: 0.2,
+    },
+  },
+  district_subdivisions: {
+    table: districtSubdivisions,
+    idColumn: districtSubdivisions.id,
+    geometryColumn: districtSubdivisions.geom,
+    defaultStyle: {
+      color: "#ff7800",
+      weight: 2,
+      opacity: 0.8,
+      fillColor: "#ff7800",
+      fillOpacity: 0.2,
+    },
+  },
+  education_facilities: {
+    table: educationFacilities,
+    idColumn: educationFacilities.id,
+    geometryColumn: educationFacilities.geom,
+    defaultStyle: {
+      color: "#555555",
+      weight: 3,
+      opacity: 0.7,
+    },
+  },
+  populated_places: {
+    table: populatedPlaces,
+    idColumn: populatedPlaces.id,
+    geometryColumn: populatedPlaces.geom,
+    defaultStyle: {
+      color: "#555555",
+      weight: 3,
+      opacity: 0.7,
+    },
+  },
+  roads: {
+    table: roads,
+    idColumn: roads.id,
+    geometryColumn: roads.geom,
+    defaultStyle: {
+      color: "#555555",
+      weight: 3,
+      opacity: 0.7,
+    },
+  },
+}
