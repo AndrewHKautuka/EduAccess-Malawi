55cedc25ac791a9eee5a77a2bddd6f63ef0f46fe
AndrewHKautuka <andrew.h.kautuka@gmail.com>
2025-12-03 06:38:08 +0200

    Add BBOX_FILTERED_LAYERS constant for bounding box layer filtering
    - Introduced a new constant, BBOX_FILTERED_LAYERS, to define layers that should be filtered by bounding box, enhancing spatial data management in the Malawi map module.
    - This addition supports improved layer visibility and interaction based on geographic boundaries.

--- src/modules/map/constants/malawi-map-constants.ts
+++ src/modules/map/constants/malawi-map-constants.ts
@@ -1,5 +1,7 @@
 import { LatLngBoundsExpression, LatLngExpression } from "leaflet"
 
+import { AVAILABLE_LAYERS } from "@/shared/constants/layer-constants"
+
 export const MAP_INITIAL_POSITION: LatLngExpression = [-13.25, 34.3]
 export const MAP_BOUNDS: LatLngBoundsExpression = [
   [-9.25, 32],
@@ -9,3 +11,10 @@ export const MAP_MIN_ZOOM: number = 7
 export const MAP_INITIAL_ZOOM: number = MAP_MIN_ZOOM
 
 export const MAP_RELOAD_LAYER_DEBOUNCE_PERIOD: number = 250
+
+// Layers that should be filtered by bounding box
+export const BBOX_FILTERED_LAYERS = new Set<keyof typeof AVAILABLE_LAYERS>([
+  "education_facilities",
+  "populated_places",
+  "roads",
+])
